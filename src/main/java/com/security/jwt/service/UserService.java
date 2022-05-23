346cbabc36003196c049d0f3f40c009ebd255eb6
safrii <you@example.com>
2022-05-23 23:01:59 +0200

    adding model, repository and service for user and roles

---
+++ src/main/java/com/security/jwt/service/UserService.java
@@ -0,0 +1,19 @@
+package com.security.jwt.service;
+
+import com.security.jwt.model.Role;
+import com.security.jwt.model.User;
+
+import java.util.List;
+
+public interface UserService {
+
+    User saveUser(User user);
+
+    Role saveRole(Role role);
+
+    void addRoleToUser(String username, String roleName);
+
+    User getUser(String username);
+
+    List<User> getUsers();
+}
---
+++ src/main/java/com/security/jwt/service/impl/UserServiceImpl.java
@@ -0,0 +1,51 @@
+package com.security.jwt.service.impl;
+
+import com.security.jwt.model.Role;
+import com.security.jwt.model.User;
+import com.security.jwt.repository.RoleRepository;
+import com.security.jwt.repository.UserRepository;
+import com.security.jwt.service.UserService;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.List;
+
+@Service
+@Transactional
+public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+    private final RoleRepository roleRepository;
+
+    public UserServiceImpl(UserRepository userRepository, RoleRepository roleRepository) {
+        this.userRepository = userRepository;
+        this.roleRepository = roleRepository;
+    }
+
+    @Override
+    public User saveUser(User user) {
+        return userRepository.save(user);
+    }
+
+    @Override
+    public Role saveRole(Role role) {
+        return roleRepository.save(role);
+    }
+
+    @Override
+    public void addRoleToUser(String username, String roleName) {
+        User user = userRepository.findByUsername(username);
+        Role role = roleRepository.findByName(roleName);
+        user.getRoles().add(role);
+    }
+
+    @Override
+    public User getUser(String username) {
+        return userRepository.findByUsername(username);
+    }
+
+    @Override
+    public List<User> getUsers() {
+        return userRepository.findAll();
+    }
+}
